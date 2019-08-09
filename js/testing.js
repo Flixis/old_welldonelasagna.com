8c0d383de39e097681c685415b35c1a0844cfaeb
Tariq <tariq9853@gmail.com>
2019-08-09 03:40:09 +0200

    Learning to use typescipt

---
+++ js/testing.js
@@ -0,0 +1,7 @@
+var a = 5;
+var b = 5;
+var c = a + b;
+console.log(c);
+var message = "Hello World";
+console.log(message);
+//# sourceMappingURL=testing.js.map
---
+++ js/testing.ts
@@ -0,0 +1,7 @@
+let a = 5;  
+let b = 5;  
+let c = a + b;
+console.log(c);
+
+let message : string = "Hello World";
+console.log(message);
