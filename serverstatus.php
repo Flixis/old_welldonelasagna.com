facb490d864a7153cf61f0d0a51738625e2a480d
Tariq <tariq9853@gmail.com>
2019-08-20 20:27:43 +0200

    More php efforts

--- serverstatus.php
+++ serverstatus.php
@@ -1,11 +1,20 @@
 <?php
-$host = '193.33.186.70'; 
+print("this page functions");
+echo"<br>";
+echo"<br>";
+
+$host = '172.217.168.206'; 
 $port = 80; 
-$waitTimeoutInSeconds = 1; 
-if($fp = fsockopen($host,$port,$errCode,$errStr,$waitTimeoutInSeconds)){   
-   // It worked 
-} else {
-   // It didn't work 
-} 
+$waitTimeoutInSeconds = 1;
+if(isset($_POST['servercheck'])){
+   if($fp = fsockopen($host,$port,$errCode,$errStr,$waitTimeoutInSeconds)){   
+      // It worked
+      print("it worked");
+   } else {
+      // It didn't work 
+      print("it didn't work");
+   } 
+}
 fclose($fp);
+
 ?>
