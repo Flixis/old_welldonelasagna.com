6e2d5d8495969fc182a594bb521b17e2be3acbb1
Tariq <tariq9853@gmail.com>
2020-05-08 23:57:55 +0200

    Removed unused content.

--- js/testing.js
+++
@@ -1,7 +0,0 @@
-var a = 5;
-var b = 5;
-var c = a + b;
-console.log(c);
-var message = "Hello World";
-console.log(message);
-//# sourceMappingURL=testing.js.map
--- js/testing.ts
+++
@@ -1,9 +0,0 @@
-let a = 5;  
-let b = 5;  
-let c = a + b;
-console.log(c);
-
-let message : string = "Hello World";
-console.log(message);
-
-
